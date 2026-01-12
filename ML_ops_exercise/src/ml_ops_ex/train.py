2f9834af30a2b972f623a3e1847880b7ed4d44a9
CAN103 <canickelsen@gmail.com>
2026-01-12 17:13:46 +0100

    Fix imports in train.py to use package path

--- ML_ops_exercise/src/ml_ops_ex/train.py
+++ ML_ops_exercise/src/ml_ops_ex/train.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import torch
 import typer
-from data import corrupt_mnist
-from model import Model as MyAwesomeModel
+from ml_ops_ex.data import corrupt_mnist
+from ml_ops_ex.model import Model as MyAwesomeModel
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
